517103c7a4043afb7003d72fa15101d93c4d5e8c
Saket Kumawat <98548534+Saket-Kumawat@users.noreply.github.com>
2025-06-02 16:50:46 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+var urlDB = make(map[string]URL)
+
+func generateShortURL(originalURL string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(originalURL))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return hash[:8]
+}
+
+func createURL(originalURL string) string {
+	shortURL := generateShortURL(originalURL)
+	id := shortURL
+	urlDB[id] = URL{
+		ID:           id,
+		OriginalURL:  originalURL,
+		ShortURL:     shortURL,
+		CreationDate: time.Now(),
+	}
+	return shortURL
+}
+
+func getURL(id string) (URL, error) {
+	url, ok := urlDB[id]
+	if !ok {
+		return URL{}, errors.New("URL not found")
+	}
+	return url, nil
+}
+
+func shortURLHandler(w http.ResponseWriter, r *http.Request) {
+	enableCORS(&w)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data struct {
+		URL string `json:"url"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	short := createURL(data.URL)
+
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: short}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/redirect/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+}
+
+func staticFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, filepath.Join("static", "index.html"))
+}
+
+func enableCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/", staticFileHandler)
+	http.HandleFunc("/shorten", shortURLHandler)
+	http.HandleFunc("/redirect/", redirectHandler)
+
+	fmt.Printf("Server running on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+}
